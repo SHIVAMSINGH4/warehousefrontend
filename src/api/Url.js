b3eee07beb5222209d120f478f7c008774809898
heavyprogrammer7@gmail.com <heavyprogrammer7@gmail.com>
2023-03-04 18:33:46 +0530

    fetching data from server

--- src/api/Url.js
+++
@@ -1,8 +0,0 @@
-export const API_URL = {
-    // BASIC_URL:"https://node-api91.herokuapp.com",
-    // AUTH_URL:"https://node-api91.herokuapp.com/auth/",
-    // EMPLOYEE_URL:"https://node-api91.herokuapp.com/crud/"
-    
-    Auth_URL : "192.168.0.103:8080/v1/api/auth/",
-
-}
